04c9501af7afa10f98ebaf4c210c0af17450eeb9
Tiago <tiagotavarescavalheiro@gmail.com>
2026-06-22 00:25:23 -0300

    Changing menu interations Creating ABC class Creating Factory Level 1 Bg implementation: parallax

---
+++ code/Game.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import pygame
+from code.Cons import WIN_WIDTH, WINDOW_HEIGHT, MENU_OPTION
+from code.Level import Level, Level
+from code.Menu import Menu
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WINDOW_HEIGHT))
+
+    def run(self):
+        while True:
+            menu = Menu(self.window)
+            menu_return = menu.run()
+
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                level = Level(self.window, 'Level 1', menu_return)
+                level_return = level.run()
+            elif menu_return == MENU_OPTION[4]:
+                pygame.quit()  # Close window
+                quit()  # End Game
+            else:
+                pass
+
---
+++ code/Level.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import pygame
+from code.EntityFactory import EntityFactory
+from code.Entity import Entity
+
+
+class Level:
+    def __init__(self, window, name, game_mode):
+        self.window = window
+        self.name = name
+        self.game_mode = game_mode
+        self.entity_list = list[Entity] = []
+
+    def run(self, ):
+        pass
+
+class Level:
+    def __init__(self, window, name, game_mode):
+        self.window = window
+        self.name = name
+        self.game_mode = game_mode
+        self.entity_list : list[Entity] = []
+        self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
+
+
+    def run(self):
+        while True:
+            for ent in self.entity_list:
+                self.window.blit(source=ent.surf, dest=ent.rect)
+                ent.move()
+            pygame.display.flip()
+        pass
