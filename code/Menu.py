d8e4c6119256f2d3faa08edafa86e724377b53a6
Tiago <tiagotavarescavalheiro@gmail.com>
2026-06-22 22:48:30 -0300

    Fixing imports news in Level.py file

--- code/Menu.py
+++ code/Menu.py
@@ -11,7 +11,7 @@ from code.Cons import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_Y
 class Menu:
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load("./asset/MenuBg.png")
+        self.surf = pygame.image.load("./asset/MenuBg.png").convert_alpha()
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
